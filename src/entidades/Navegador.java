b0e4bcda08ada3668f6aa5e416f2a88b3db048fd
Cleeff-Daniel-Delacruz-Tirado <129539428+Cleeff-Daniel-Delacruz-Tirado@users.noreply.github.com>
2023-05-28 22:56:10 -0500

    recordatorio de la clase
    interfaz: Todas las clases tienn el mismo contrato , obligado a implementar metodos de esa interfaz. Sin necesidad de heredar de una SuprClass

---
+++ src/entidades/Navegador.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidades;
+
+/**
+ *
+ * @author Estudiante
+ */
+public interface Navegador {
+    
+    public void navegar();
+    public void abrirLink();
+    public String getNombre();
+    //obligandolo a que tenga todos nombre por ser navegadores
+    
+}
